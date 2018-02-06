9213eeb956fdc688ee7944238f363a4a6c0718d3
wpumacay <wpumacay@gmail.com>
2018-02-06 13:22:35 -0500

    added kinect link to script

--- res/get_calibration_files.sh
+++ res/get_calibration_files.sh
@@ -3,7 +3,8 @@
 echo "GETTING CALIBRATION FILES FROM DROPBOX"
 
 FILES=( "https://www.dropbox.com/s/yjip8kscakq257l/calibration_mslifecam.avi"  
-        "https://www.dropbox.com/s/so7qjmpusrpmurr/calibration_ps3eyecam.avi" )
+        "https://www.dropbox.com/s/so7qjmpusrpmurr/calibration_ps3eyecam.avi"
+        "https://www.dropbox.com/s/8p5nuvjcnyxkkv6/calibration_kinectv2.avi" )
 
 ## get the files from dropbox
 for file in "${FILES[@]}"
